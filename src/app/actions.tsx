f8fd655212bb2fc5195b03e3ea847f4710cc5c75
HaJaehyeong <hajae305@gmail.com>
2024-11-08 10:51:18 +0900

    Next.js Routing: [WIP] Redirecting server,client

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,7 +1,12 @@
 'use server';
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-const createUser = async (prevState: any, formData: FormData) => {
+export type CreateUserState = {
+  message: string;
+};
+
+const createUser = async (prevState: CreateUserState, formData: FormData) => {
   const id = formData.get('id') as string;
   const password = formData.get('password') as string;
   const name = formData.get('name') as string;
@@ -19,7 +24,8 @@ const createUser = async (prevState: any, formData: FormData) => {
     return { message: errorMessage.message };
   }
 
+  revalidatePath('/dashboard');
   redirect('/dashboard');
 };
 
-export { createUser };
+export default createUser;
--- src/app/ui/signup.tsx
+++ src/app/ui/signup.tsx
@@ -1,25 +1,33 @@
 'use client';
-
-import { createUser } from '@/app/actions';
+import { useRouter } from 'next/navigation';
 import { useActionState } from 'react';
+import createUser, { CreateUserState } from '../actions';
 
-const initialState = {
+const initialCreateUserState: CreateUserState = {
   message: 'Hello Mr.My Yesterday time machine을 타고',
 };
 
 export function Signup() {
-  const [state, formAction] = useActionState(createUser, initialState);
+  const [state, formAction] = useActionState(createUser, initialCreateUserState);
+
+  // NOTE(hajae): 클라이언트 컴포넌트의 이벤트 핸들러 내에서 리디렉션이 필요한 경우 useRouter훅의 push 메서드를 사용
+  const router = useRouter();
 
   return (
-    <form action={formAction}>
-      <label htmlFor="id">ID</label>
-      <input type="text" id="id" name="id" required />
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" name="password" />
-      <label htmlFor="name">Name</label>
-      <input type="name" id="name" name="name" />
-      <p aria-live="polite">{state?.message}</p>
-      <button>Sign up</button>
-    </form>
+    <>
+      <form action={formAction}>
+        <label htmlFor="id">ID</label>
+        <input type="text" id="id" name="id" required />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <label htmlFor="name">Name</label>
+        <input type="name" id="name" name="name" />
+        <p aria-live="polite">{state.message}</p>
+        <button>Sign up</button>
+      </form>
+      <button type="button" onClick={() => router.push('/dashboard')}>
+        To Dashboard
+      </button>
+    </>
   );
 }
